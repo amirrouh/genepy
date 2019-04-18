ae19e9e48f973498c315f07235c21d7a8c82b139
Amir Rouhollahi <43451887+amirrouh@users.noreply.github.com>
2019-04-18 18:22:00 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,51 @@
+def connect(directory_1, directory_2, key_address, user, server):
+    """
+    This modules creates a bash file to connect connect the to UCF clusters 
+
+
+    Parameters
+    ----------
+    directory_1 : str
+        This arg gives the folder we want to sync on local machine
+        (1.e. '~/Dropbox/python/')
+    directory_2 : str
+        destination address on the server 
+        (i.e. '~/Dropbox/')
+    key_address : str
+        This are gets full address of the secure key file 
+        (i. e. '~/Dropbox/stokes/arouhollahi-keys/arouhollahi_id_rsa_1')
+    user : str
+        Username
+        
+    server: str
+        Server address
+        (i.e. )
+
+    -----------
+    returns:
+        connect.sh bash file can be run using sh connect.sh in terminal 
+        to connect to the UCF clusters
+    """
+
+    import os
+    #   Creates a list of files in the working directory
+    files = os.listdir()
+
+    # If the bash file already exists, it deletes the bash file before making progress
+    if 'connect.sh' in files: 
+        os.remove('connect.sh')
+    else:
+        pass
+
+    with open('connect.sh', 'w') as f:
+        f.write('#!/bin/bash\n')
+        f.write('ssh -Y -i ' + str(key_address) + ' ' + str(user) + \
+            '@' + str(server))
+
+from inputs import *
+connect(directory_1, directory_2, key_address, user, server)
+
+
 def sync(directory_1, directory_2, key_address, user, server):
     """
     This module creates a bash script to compressed desired files and folders and copies 

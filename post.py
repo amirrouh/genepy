c3174528a38152dcbcc120e395e3533fb9393ebf
Amir Rouhollahi <43451887+amirrouh@users.noreply.github.com>
2019-04-20 14:58:04 -0400

    Update post.py

--- post.py
+++ post.py
@@ -1,6 +1,39 @@
 """
 This module includes post processing functions
 """
+import numpy as np
+from sensitivity import change_var
+from matplotlib import pyplot as plt
+
+#	Tesing variables are defined here 
+def var_database():
+	"""
+	This function stores variable and their ranges
+
+	Parameters
+	-----
+	None
+
+	Returns
+	-----
+	test_samples : dic
+		Gives variables and values used for testing
+	"""
+	test_samples = [{'input_dim': range(2000,22000,2000)},
+			{'epoch': range(50, 550, 10)},
+			{'batch': range(500, 5500, 500)},
+			{'latent': range(10,110,10)},
+			{'encoder_i': range(100,1050,50)},
+			{'encoder_o': range(100,1050,50)},
+			{'decoder_i': range(100,1050,50)},
+			{'decoder_o': range(100,1050,50)},
+			{'train_percent': range(50,95,5)},
+			{'lam': np.logspace(0.1, 1e-5, 10, endpoint=True)},
+			{'c': np.logspace(1, 1e5, 10, endpoint=True)},
+			{'norm_order': range(5, 50, 5)}]
+
+	return test_samples
+
 
 def test():
 	"""
@@ -13,21 +46,59 @@ def test():
 
 	Returns
 	-----
-	Export the results at .txt files in the temp folder
+	Saves *.txt files in temp folder including the sensitivity analyisis results
 	"""
-	from sensitivity import change_var
-	import numpy as np
-
-	test_samples = [{'input_dim': range(2000,22000,2000)},
-		    {'epoch': range(50, 550, 10)},
-		    {'batch': range(500, 5500, 500)},
-		    {'latent': range(10,110,10)},
-		    {'encoder_i': range(100,1050,50)},
-		    {'encoder_o': range(100,1050,50)},
-		    {'decoder_i': range(100,1050,50)},
-		    {'decoder_o': range(100,1050,50)},
-		    {'train_percent': range(50,95,5)},
-		    {'lam': np.logspace(0.1, 1e-5, 10)},
-		    {'c': np.logspace(1, 1e5, 10)},
-		    {'norm_order': range(5, 50, 5)}]
+	test_samples = var_database()
 	change_var(test_samples)
+
+	return test_samples
+
+
+def plot(key):
+	"""
+	This function plots the sensitivity analysis results.
+
+	Parameters
+	-----
+	key : str
+		name of variable to be plotted
+
+	Returns
+	-----
+	Saves key.png images in temp folder 
+	"""
+	#	Get the values of key in the var_database
+	for item in var_database():
+		if list(item.keys())[0] == key:
+			values = item[key]
+
+	#	Read the file corresponding to the variable
+	file = np.genfromtxt('temp/'+ key + '_change_out.txt', dtype=float, delimiter=' ')
+
+	#   Whole model runtime is summation of autoencoder and SVM runtime
+	runtime = file[0,:] + file[2,:]
+
+	#   Plot compression error (%)
+	plt.subplot(1,3,1)
+	plt.plot(values, file[1,:])
+	plt.xlabel(key)
+	plt.ylabel('Compression error (%)')
+
+	#   Plot Classification error (%)
+	plt.subplot(1,3,2)
+	plt.plot(values, file[3,:])
+	plt.xlabel(key)
+	plt.ylabel('Classification error (%)')
+
+	#   Plot Classification error (%)
+	plt.subplot(1,3,3)
+	plt.plot(values, runtime)
+	plt.xlabel(key)
+	plt.ylabel('Model runtime (sec)')
+
+	#	Adjust, save and showing the plot
+	plt.subplots_adjust(bottom =0.25, top=0.75, wspace=1)
+	plt.savefig('temp/' + key + '.png')
+	plt.show()
+
+	return

dde1362ca0c6bcbd0ae8114ac99ae28324e65b99
Amir Rouhollahi <43451887+amirrouh@users.noreply.github.com>
2019-04-21 16:53:34 -0400

    Add files via upload

--- post.py
+++ post.py
@@ -4,28 +4,25 @@ This module includes post processing functions
 import numpy as np
 from sensitivity import test
 from matplotlib import pyplot as plt
-from parameters import parametrize
+from parameters import database
 
-def plot(key):
+def plot_run(parameter, values):
 	"""
 	This function plots the sensitivity analysis results.
 
 	Parameters
 	-----
-	key : str
+	parameter : str
 		name of variable to be plotted
+	values: list
+		list of values for the above variable
 
 	Returns
 	-----
 	Saves key.png images in temp folder 
 	"""
-	#	Get the values of key in the var_db
-	for item in var_info()[0]:
-		if list(item.keys())[0] == key:
-			values = item[key]
-
 	#	Read the file corresponding to the variable
-	file = np.genfromtxt('temp/'+ key + '_change_out.txt', dtype=float, delimiter=' ')
+	file = np.genfromtxt('temp/'+ parameter + '_change_out.txt', dtype=float, delimiter=' ')
 
 	#   Whole model runtime is summation of autoencoder and SVM runtime
 	runtime = file[0,:] + file[2,:]
@@ -33,24 +30,31 @@ def plot(key):
 	#   Plot compression error (%)
 	plt.subplot(1,3,1)
 	plt.plot(values, file[1,:])
-	plt.xlabel(key)
+	plt.xlabel(parameter)
 	plt.ylabel('Compression error (%)')
 
 	#   Plot Classification error (%)
 	plt.subplot(1,3,2)
 	plt.plot(values, file[3,:])
-	plt.xlabel(key)
+	plt.xlabel(parameter)
 	plt.ylabel('Classification error (%)')
 
 	#   Plot Classification error (%)
 	plt.subplot(1,3,3)
 	plt.plot(values, runtime)
-	plt.xlabel(key)
+	plt.xlabel(parameter)
 	plt.ylabel('Model runtime (sec)')
 
 	#	Adjust, save and showing the plot
 	plt.subplots_adjust(bottom =0.25, top=0.75, wspace=1)
-	plt.savefig('temp/' + key + '.png')
+	plt.savefig('temp/' + parameter + '.png')
 	plt.show()
 
 	return ()
+
+
+def plot(test_params):
+	for item in test_params:
+		key = list(item.keys())[0]
+		values = list(item.values())[0]
+		plot_run(key, values)
